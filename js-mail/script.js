9a007a4de20435563a80b4e9478d005e2f8540c8
Andrea Barbuto <barbutoandrea99@gmail.com>
2025-12-04 16:30:17 +0100

    feat: added script index.html for js-mail

---
+++ js-mail/script.js
@@ -0,0 +1,29 @@
+/* Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+Si può fare? Certo che si basta ragionare un po’.
+Nota: 
+Non è necessario provvedere alla validazione delle email */
+
+
+const acceptedEmail = ["test@gmail.com", "test2@gmail.com", "test3@gmail.com"];
+let userEmail = prompt("Inserisci la tua email");
+
+for (let i = 0; i < acceptedEmail.length; i++) {
+  
+    if(userEmail === acceptedEmail[i]) {
+        console.log("La tua email è accettata"); 
+        i = acceptedEmail.length;
+    }
+
+    else {
+        console.log("La tua email non è presente");
+    }
+}
+
+
+
+
+
